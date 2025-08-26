44a8325d40b572fc3163a8ec37c2347fc81a80f8
ShantiZinchiri <shantizito@hotmail.com>
2025-08-26 13:14:01 -0600

    add new data

---
+++ data.py
@@ -0,0 +1,6 @@
+urban_routes_url = 'https://cnt-5f6fcf5b-9506-4997-b423-f3148b201f57.containerhub.tripleten-services.com/'
+address_from = 'East 2nd Street, 601'
+address_to = '1300 1st St'
+phone_number = '+1 123 123 12 12'
+card_number, card_code = '1234 5678 9100', '111'
+message_for_driver = 'Muéstrame el camino al museo'
